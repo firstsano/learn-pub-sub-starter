e512611ed59def8110f808a5566b74b3152993c0
Aleksandr Koltunov <firstsano@gmail.com>
2025-10-11 11:44:23 +0500

    feat: add publishing JSON

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -16,6 +18,19 @@ func main() {
 		log.Fatal(err)
 	}
 	defer rabbit.Close()
+
+	channel, err := rabbit.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer channel.Close()
+
+	err = pubsub.PublishJSON(channel, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{
+		IsPaused: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println("Connected to RabbitMQ")
 	fmt.Println("Starting Peril server...")
