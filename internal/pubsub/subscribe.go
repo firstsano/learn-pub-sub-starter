4c4753436509112ae891a3cfd17d71fc283f8f8f
Aleksandr Koltunov <firstsano@gmail.com>
2025-11-05 15:17:12 +0500

    feat: add war logs

---
+++ internal/pubsub/subscribe.go
@@ -0,0 +1,108 @@
+package pubsub
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func SubscribeJSON[T any](
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueue, // an enum to represent "durable" or "transient"
+	handler func(T) AckType,
+) error {
+	channel, queue, err := DeclareAndBind(conn, exchange, queueName, key, queueType)
+	if err != nil {
+		return fmt.Errorf("failed to subscribe to queue %s: %w", queueName, err)
+	}
+
+	subs, err := channel.Consume(
+		queue.Name, // queue
+		"",         // consumer
+		false,      // auto-ack
+		false,      // exclusive
+		false,      // no-local
+		false,      // no-wait
+		nil,        // args
+	)
+	if err != nil {
+		return fmt.Errorf("failed to consume: %w", err)
+	}
+
+	go readDeliveries(channel, subs, handler)
+
+	return nil
+}
+
+func readDeliveries[T any](
+	channel *amqp.Channel,
+	deliveries <-chan amqp.Delivery,
+	handler func(T) AckType,
+) {
+	defer channel.Close()
+	defer fmt.Print("> ")
+
+	var message T
+	fmt.Println("Reading messages...")
+
+	for delivery := range deliveries {
+		err := json.Unmarshal(delivery.Body, &message)
+		if err != nil {
+			log.Println("failed to unmarshal message:", err)
+			continue
+		}
+
+		switch ack := handler(message); ack {
+		case Ack:
+			log.Println("Acknowledge message")
+			_ = delivery.Ack(false)
+		case NackRequeue:
+			log.Println("Requeue message")
+			_ = delivery.Nack(false, true)
+		case NackDiscard:
+			log.Println("Discard message")
+			_ = delivery.Nack(false, false)
+		default:
+			log.Println("Unknown ack type:", ack)
+			log.Println("Skipping message")
+		}
+	}
+}
+
+func DeclareAndBind(
+	conn *amqp.Connection,
+	exchange,
+	queueName,
+	key string,
+	queueType SimpleQueue,
+) (*amqp.Channel, amqp.Queue, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	q, err := ch.QueueDeclare(
+		queueName,
+		queueType == SimpleQueueDurable,
+		queueType == SimpleQueueTransient,
+		queueType == SimpleQueueTransient,
+		false,
+		amqp.Table{
+			"x-dead-letter-exchange": DeadLettersExchange,
+		},
+	)
+	if err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	if err = ch.QueueBind(queueName, key, exchange, false, nil); err != nil {
+		return nil, amqp.Queue{}, err
+	}
+
+	return ch, q, nil
+}
